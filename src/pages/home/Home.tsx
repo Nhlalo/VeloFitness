6a54e0465cc538949bfebd684e7a044f47ba3c60
Nhlalo <nhlalonkosi@gmail.com>
2026-03-06 18:43:09 +0200

    feat: create basic structure of the hero banner component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,27 @@
+// import Container from "../../components/shared/Container";
+import WorkOutVideo from "../../assets/videos/herobanner.mp4";
+
+function HeroBanner() {
+  return (
+    <section>
+      <video
+        src={WorkOutVideo}
+        autoPlay
+        muted
+        loop
+        playsInline
+        disablePictureInPicture
+        controls={false}
+      >
+        Your browser does not support the video tag
+      </video>
+    </section>
+  );
+}
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <HeroBanner />
+    </>
+  );
 }
