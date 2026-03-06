c618b49ea700b1c13540f44017079b71d8cf3c04
Nhlalo <nhlalonkosi@gmail.com>
2026-03-06 15:17:33 +0200

    feat: add code to make TypeScript process media files

---
+++ src/media.d.ts
@@ -0,0 +1,65 @@
+/*TypeScript cannot process media types, so this notifies Typescript to treat importing a media file as string (the file path) */
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mp4" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.webm" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mov" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.avi" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.m4v" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.ogv" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mkv" {
+  const value: string;
+  export default value;
+}
