c24f90054dfc583217ac5de0934b51c3787d3968
Nhlalo <nhlalonkosi@gmail.com>
2026-03-10 13:20:26 +0200

    feat: create header component for the clubs component

--- src/pages/clubs/Clubs.tsx
+++ src/pages/clubs/Clubs.tsx
@@ -1,3 +1,21 @@
+import { Search } from "lucide-react";
+import { acquireNumberGyms } from "../../data/constants/gymlocation";
+
+function Header() {
+  return (
+    <header>
+      <h1>{acquireNumberGyms()} Clubs Worldwide: Find a Club Near You</h1>
+      <div>
+        <Search aria-hidden="true" />
+        <input type="text" />
+      </div>
+    </header>
+  );
+}
 export default function Clubs() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
