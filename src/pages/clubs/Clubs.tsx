6e83761dc263e5e08fd2b035eea458e2fe2bdc66
Nhlalo <nhlalonkosi@gmail.com>
2026-03-11 20:37:33 +0200

    style: implement styling to the header component

--- src/pages/clubs/Clubs.tsx
+++ src/pages/clubs/Clubs.tsx
@@ -3,11 +3,22 @@ import { acquireNumberGyms } from "../../data/constants/gymlocation";
 
 function Header() {
   return (
-    <header>
-      <h1>{acquireNumberGyms()} Clubs Worldwide: Find a Club Near You</h1>
-      <div>
-        <Search aria-hidden="true" />
-        <input type="text" />
+    <header className="flex justify-center bg-black pb-22 text-white">
+      <div className="flex w-[90%] flex-col justify-center">
+        <h1 className="text-semibold mb-6 flex items-center pt-22 text-4xl uppercase lg:text-6xl">
+          {acquireNumberGyms()} Clubs Worldwide: Find A Club Near You
+        </h1>
+        <div className="relative">
+          <Search
+            aria-hidden="true"
+            className="text-6 absolute top-1/2 ml-4 -translate-y-1/2 text-white/30"
+          />
+          <input
+            type="text"
+            className="flex h-16 w-full min-w-9 items-center rounded-full border-2 border-solid border-white/50 px-14 text-base text-white lg:text-lg"
+            placeholder="City, ZIP Code, or Postal Code"
+          />
+        </div>
       </div>
     </header>
   );
