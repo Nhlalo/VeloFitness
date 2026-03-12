d85e672560aa81a92069d4ae097319fbec454180
Nhlalo <nhlalonkosi@gmail.com>
2026-03-12 13:54:00 +0200

    styling: add margin bottom to the heading of the header component

--- src/pages/clubs/Clubs.tsx
+++ src/pages/clubs/Clubs.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header className="flex justify-center bg-black pb-22 text-white">
       <div className="flex w-[90%] flex-col justify-center">
-        <h1 className="mb-6 flex items-center pt-22 text-4xl font-semibold uppercase lg:text-[4rem]">
+        <h1 className="mb-12 flex items-center pt-22 text-4xl font-semibold uppercase lg:text-[4rem]">
           {acquireNumberGyms()} Clubs Worldwide:{" "}
           <br aria-hidden="true" className="hidden lg:block" /> Find A Club Near
           You
@@ -31,47 +31,49 @@ function Header() {
 function GymLocations() {
   return (
     <Container>
-      {gymDescription.map((content) => {
-        const linkDescription = `view the information about the ${content.location} clubs`;
-        return (
-          <Link
-            to=""
-            aria-label={linkDescription}
-            key={content.location}
-            className="flex px-6 py-8 text-white hover:bg-white hover:text-black"
-          >
-            <div className="flex grow-2 flex-col items-center lg:flex-1">
-              <span className="w-full text-xl lg:text-2xl">
-                {content.location}
-              </span>
-              <p className="pt-4 pr-6 text-sm">{content.description}</p>
-              <div className="flex items-center self-start pt-4 text-sm">
-                View all {content.clubs.length} clubs{" "}
-                <ArrowRight aria-hidden="true" />
+      <div className="pb-24">
+        {gymDescription.map((content) => {
+          const linkDescription = `view the information about the ${content.location} clubs`;
+          return (
+            <Link
+              to=""
+              aria-label={linkDescription}
+              key={content.location}
+              className="flex border-t border-solid border-white px-6 py-8 text-white hover:bg-white hover:text-black"
+            >
+              <div className="flex grow-2 flex-col items-center lg:flex-1">
+                <span className="w-full text-xl lg:text-2xl">
+                  {content.location}
+                </span>
+                <p className="pt-4 pr-6 text-sm">{content.description}</p>
+                <div className="flex items-center self-start pt-4 text-sm font-semibold">
+                  View all {content.clubs.length} clubs{" "}
+                  <ArrowRight aria-hidden="true" />
+                </div>
               </div>
-            </div>
-            <div className="hidden flex-1 flex-col items-center lg:flex">
-              <span className="mr-24 text-sm">Featured Clubs</span>
-              <ul className="list-disc">
-                {content.clubs.map((clubs) => {
-                  return (
-                    <li key={clubs.name} className="text-sm">
-                      {clubs.name}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div className="hidden aspect-3/2 grow flex-col items-center md:flex lg:flex-1">
-              <img
-                src={content.image}
-                alt="Gym club"
-                className="h-full w-full object-cover"
-              />
-            </div>
-          </Link>
-        );
-      })}
+              <div className="hidden flex-1 flex-col items-center lg:flex">
+                <span className="mr-24 text-sm">Featured Clubs</span>
+                <ul className="list-disc">
+                  {content.clubs.map((clubs) => {
+                    return (
+                      <li key={clubs.name} className="text-sm">
+                        {clubs.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="hidden aspect-3/2 grow flex-col items-center md:flex lg:flex-1">
+                <img
+                  src={content.image}
+                  alt="Gym club"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </Container>
   );
 }
