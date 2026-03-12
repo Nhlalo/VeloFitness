4e4dbcbae349c8b516de5ef72a302f3c15c29d29
Nhlalo <nhlalonkosi@gmail.com>
2026-03-12 21:45:10 +0200

    feat: create error component

---
+++ src/pages/clubs/Result.tsx
@@ -0,0 +1,13 @@
+import { LoaderCircle } from "lucide-react";
+import { Gym, Description } from "../../types/club.interface";
+
+function Error() {
+  return (
+    <div>
+      <p>
+        An unexpected error occurred. Please refresh the page and try again. If
+        the problem persists, contact support.
+      </p>
+    </div>
+  );
+}
