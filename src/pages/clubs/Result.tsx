f6c8cab1dd9ba040c0a124561dcf7d943049c7bd
Nhlalo <nhlalonkosi@gmail.com>
2026-03-12 21:46:05 +0200

    feat: create data component

--- src/pages/clubs/Result.tsx
+++ src/pages/clubs/Result.tsx
@@ -18,3 +18,52 @@ function Loading() {
     </div>
   );
 }
+
+function Data({ data }: { data: Gym[] | Description[] | string }) {
+  return (
+    <>
+      {typeof data !== "string" &&
+        data?.map((content) => {
+          const isGym = "name" in content;
+          const isDescription = "country" in content;
+
+          const displayName = isGym
+            ? content.name
+            : isDescription
+              ? content.country
+              : "";
+
+          const key = isGym
+            ? content.name
+            : isDescription
+              ? content.country
+              : "unknown";
+
+          return (
+            <button
+              type="button"
+              className="flex w-full gap-6 bg-[#282828] px-6 py-8"
+              key={key}
+            >
+              <div className="aspect-2/1 w-14" aria-hidden="true">
+                <img
+                  src={content.image}
+                  alt="Gym Club"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div className="flex grow flex-col items-start">
+                <span>{displayName}</span>
+                <span>Club</span>
+              </div>
+            </button>
+          );
+        })}
+      {typeof data == "string" && (
+        <div>
+          <p>{data}</p>
+        </div>
+      )}
+    </>
+  );
+}
