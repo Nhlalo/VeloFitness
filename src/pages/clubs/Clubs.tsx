e3e7ae29c6273ed0458e88568bbef198fc3d35c2
Nhlalo <nhlalonkosi@gmail.com>
2026-03-13 15:55:47 +0200

    refactor: remove the header component and place in its own file

--- src/pages/clubs/Clubs.tsx
+++ src/pages/clubs/Clubs.tsx
@@ -1,52 +1,10 @@
-import { useState, ChangeEvent, useMemo } from "react";
 import { Link, useNavigate } from "react-router";
-import { Search, ArrowRight } from "lucide-react";
-import { acquireNumberGyms } from "../../utils/acquireGymData";
+import { ArrowRight } from "lucide-react";
 import { gymDescription } from "../../data/constants/gymlocation";
-import debounce from "../../utils/debounce";
+import { acquireNumberGyms } from "../../utils/acquireGymData";
+import Header from "./Header";
 import Container from "../../components/shared/Container";
-import Results from "./Result";
 
-function Header() {
-  const [inputValue, setInputValue] = useState<string>("");
-
-  const debouncedSetInput = useMemo(
-    () =>
-      debounce((value: string) => {
-        setInputValue(value);
-      }, 300),
-    [],
-  );
-
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const value = e.currentTarget.value;
-    debouncedSetInput(value);
-  }
-  return (
-    <header className="flex justify-center bg-black pb-22 text-white">
-      <div className="flex w-[90%] flex-col justify-center">
-        <h1 className="mb-12 flex items-center pt-22 text-4xl font-semibold uppercase lg:text-[4rem]">
-          {acquireNumberGyms()} Clubs Worldwide:{" "}
-          <br aria-hidden="true" className="hidden lg:block" /> Find A Club Near
-          You
-        </h1>
-        <div className="relative">
-          <Search
-            aria-hidden="true"
-            className="text-6 absolute top-1/2 ml-4 -translate-y-1/2 text-white/30"
-          />
-          <input
-            type="text"
-            className="flex h-16 w-full min-w-9 items-center rounded-full border-2 border-solid border-white/50 px-14 text-base text-white lg:text-lg"
-            placeholder="City, ZIP Code, or Postal Code"
-            onChange={handleChange}
-          />
-          {inputValue.trim().length > 0 && <Results location={inputValue} />}
-        </div>
-      </div>
-    </header>
-  );
-}
 function GymLocations() {
   const navigate = useNavigate();
 
@@ -116,7 +74,11 @@ function GymLocations() {
 export default function Clubs() {
   return (
     <>
-      <Header />
+      <Header
+        heading={`${acquireNumberGyms()} Clubs Worldwide:`}
+        extraHeading="Find A Club Near
+          You"
+      />
       <main className="bg-black">
         <GymLocations />
       </main>
