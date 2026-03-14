a8f85d246ca31d118d7dca3c6522425c6471511e
Nhlalo <nhlalonkosi@gmail.com>
2026-03-14 12:17:33 +0200

    style: change the height othe section and color of the texts

--- src/pages/home/HeroBanner.tsx
+++ src/pages/home/HeroBanner.tsx
@@ -2,7 +2,7 @@ import WorkOutVideo from "../../assets/videos/herobanner.mp4";
 
 export default function HeroBanner() {
   return (
-    <section className="relative h-[88vh]">
+    <section className="relative h-[88vh] overflow-hidden bg-black">
       <video
         src={WorkOutVideo}
         autoPlay
@@ -12,18 +12,18 @@ export default function HeroBanner() {
         disablePictureInPicture
         controls={false}
         aria-hidden="true"
-        className="pointer-events-none h-full w-full scale-x-[-1] object-cover"
+        className="pointer-events-none h-full w-full scale-x-[-1] object-cover lg:h-auto"
       >
         Your browser does not support the video tag
       </video>
       <div className="absolute top-[50%] -translate-y-1/2 pl-8">
         <h1 className="flex flex-col text-5xl md:text-7xl">
-          <span className="text-[#1d1d1f]">DESIGN YOUR</span>
-          <span className="text-transparent [-webkit-text-stroke:2px_#1d1d1f]">
+          <span className="text-white">DESIGN YOUR</span>
+          <span className="text-transparent [-webkit-text-stroke:2px_white]">
             PHYSIQUE
           </span>
         </h1>
-        <button className="m-2 rounded-md bg-[#AAFF00] px-5 py-3 text-[#1d1d1f] hover:bg-[#9EF200] md:px-8 md:py-4">
+        <button className="m-2 rounded-md bg-[#AAFF00] px-5 py-3 text-white hover:bg-[#9EF200] md:px-8 md:py-4">
           GET STARTED
         </button>
       </div>
