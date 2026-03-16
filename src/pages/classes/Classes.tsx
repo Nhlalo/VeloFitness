d4b1f99c3d6c5d8002fd6f70e505e23b86737736
Nhlalo <nhlalonkosi@gmail.com>
2026-03-16 15:02:33 +0200

    refactor: use the reusable hero banner component

--- src/pages/classes/Classes.tsx
+++ src/pages/classes/Classes.tsx
@@ -1,7 +1,8 @@
-import { useNavigate } from "react-router";
 import { Props } from "../../types/classes.interface";
+import { HeroBannerProps } from "../../types/herobanner.interface";
 import Container from "../../components/shared/Container";
 import SubClasses from "../../components/shared/SubClasses";
+import HeroBanner from "../../components/shared/HeroBanner";
 import WorkOutVideo from "../../assets/videos/class-herobanner.mp4";
 import PersonalTraining from "../../assets/images/classes-personalTraining.jpg";
 import Training from "../../assets/images/classes-training.jpg";
@@ -21,50 +22,22 @@ const classesDescription: Props[] = [
   },
 ];
 
-function HeroBanner() {
-  const navigate = useNavigate();
-
-  function handleClick() {
-    navigate("/jointoday");
-  }
-
-  return (
-    <section className="relative h-screen overflow-hidden bg-black">
-      <video
-        src={WorkOutVideo}
-        autoPlay
-        muted
-        loop
-        playsInline
-        disablePictureInPicture
-        controls={false}
-        aria-hidden="true"
-        className="pointer-events-none h-full w-full scale-x-[-1] object-cover lg:h-auto"
-      >
-        Your browser does not support the video tag
-      </video>
-      <div className="absolute top-[50%] -translate-y-1/2 pl-8">
-        <h1 className="flex flex-col text-5xl md:text-7xl">
-          <span className="text-white">DESIGN FOR THE INDIVIDUAL. </span>
-          <span className="text-transparent [-webkit-text-stroke:2px_white]">
-            POWERED BY THE COLLECTIVE.
-          </span>
-        </h1>
-        <button
-          onClick={handleClick}
-          className="m-2 rounded-md bg-[#AAFF00] px-5 py-3 text-white hover:bg-[#9EF200] md:px-8 md:py-4"
-        >
-          BOOK A CLASS
-        </button>
-      </div>
-    </section>
-  );
-}
+const herobannerProps: HeroBannerProps = {
+  videoSource: WorkOutVideo,
+  heading1: "DESIGN FOR THE INDIVIDUAL.",
+  heading2: "POWERED BY THE COLLECTIVE.",
+  buttonText: "BOOK A CLASS",
+};
 
 export default function Classes() {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner
+        videoSource={herobannerProps.videoSource}
+        heading1={herobannerProps.heading1}
+        heading2={herobannerProps.heading2}
+        buttonText={herobannerProps.buttonText}
+      />
       <main className="bg-black text-white">
         {classesDescription.map((content) => {
           return (
