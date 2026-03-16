3f4e7c14ffb639eaf176c821b563f162bc611898
Nhlalo <nhlalonkosi@gmail.com>
2026-03-16 14:10:02 +0200

    feat: create a classes component to be reused multiple times

---
+++ src/components/shared/Classes.tsx
@@ -0,0 +1,42 @@
+import Container from "./Container";
+
+interface Props {
+  heading: string;
+  description: string;
+  imageSource: string;
+}
+
+export default function Classes({ heading, description, imageSource }: Props) {
+  return (
+    <Container>
+      <div className="m-auto flex max-w-285 flex-col py-18 lg:flex-row">
+        <div className="flex-1">
+          <h2 className="mt-5 mb-6 text-3xl font-bold uppercase lg:w-[50%] lg:text-4xl">
+            {heading}
+          </h2>
+          <p className="mb-6 hidden w-[60%] py-3 text-lg leading-[1.4] lg:block">
+            {description}
+          </p>
+          <button className="m-2 hidden rounded-md border-2 border-white bg-transparent px-8 py-4 text-white hover:bg-gray-400 lg:inline">
+            BOOK A CLASS
+          </button>
+        </div>
+        <div className="flex-1 overflow-hidden lg:max-w-[43%]">
+          <img
+            src={imageSource}
+            alt="training"
+            loading="lazy"
+            className="w-full"
+          />
+          <p className="mb-6 py-5 lg:hidden">
+            Science-backed classes developed by industry minds to maximize
+            transformation.
+          </p>
+          <button className="m-2 rounded-md border-2 border-white bg-transparent px-5 py-3 text-white hover:bg-gray-400 lg:hidden">
+            BOOK A CLASS
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+}
