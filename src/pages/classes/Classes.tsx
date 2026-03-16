e93818f0328b80ebbbc735f8ab6dbc98a9ebcb1d
Nhlalo <nhlalonkosi@gmail.com>
2026-03-16 11:17:35 +0200

    feat: create the hero banner component for the classes component

--- src/pages/classes/Classes.tsx
+++ src/pages/classes/Classes.tsx
@@ -1,3 +1,36 @@
+import WorkOutVideo from "../../assets/videos/class-herobanner.mp4";
+
+function HeroBanner() {
+  return (
+    <section className="relative h-screen overflow-hidden bg-black">
+      <video
+        src={WorkOutVideo}
+        autoPlay
+        muted
+        loop
+        playsInline
+        disablePictureInPicture
+        controls={false}
+        aria-hidden="true"
+        className="pointer-events-none h-full w-full scale-x-[-1] object-cover lg:h-auto"
+      >
+        Your browser does not support the video tag
+      </video>
+      <div className="absolute top-[50%] -translate-y-1/2 pl-8">
+        <h1 className="flex flex-col text-5xl md:text-7xl">
+          <span className="text-white">DESIGN FOR THE INDIVIDUAL. </span>
+          <span className="text-transparent [-webkit-text-stroke:2px_white]">
+            POWERED BY THE COLLECTIVE.
+          </span>
+        </h1>
+        <button className="m-2 rounded-md bg-[#AAFF00] px-5 py-3 text-white hover:bg-[#9EF200] md:px-8 md:py-4">
+          BOOK A CLASS
+        </button>
+      </div>
+    </section>
+  );
+}
+
 export default function Classes() {
-  return <></>;
+  return <HeroBanner />;
 }
