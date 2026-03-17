2e0216d3e1abb5504ad37d49ac5f63cc4d0122c9
Nhlalo <nhlalonkosi@gmail.com>
2026-03-17 13:22:21 +0200

    style: make width of the header wrapper be 90% while centering it using margin-auto

--- src/pages/membership/Membership.tsx
+++ src/pages/membership/Membership.tsx
@@ -1,22 +1,22 @@
-import PeopleTraining from "../../assets/images/people-training.jpg";
+import HeroBanner from "../../assets/images/membership-herobanner.jpg";
 
 function Header() {
   return (
-    <header className="relative h-screen overflow-hidden bg-black">
+    <header className="relative h-[88vh] overflow-hidden bg-black">
       <img
-        src={PeopleTraining}
+        src={HeroBanner}
         alt="Group training"
         aria-hidden="true"
         className="pointer-events-none h-full w-full scale-x-[-1] object-cover lg:h-auto"
       />
 
-      <div className="absolute top-[50%] -translate-y-1/2 pl-8">
-        <h1 className="flex flex-col text-5xl uppercase md:text-7xl">
+      <div className="w-90% absolute top-[50%] m-auto -translate-y-1/2 pl-8">
+        <h1 className="mb-10 flex flex-col text-5xl font-semibold uppercase md:text-[5rem]">
           <span className="text-white">Membership </span>
           <span className="text-white">With benefits</span>
         </h1>
 
-        <p className="text-white">
+        <p className="w-[90%] max-w-[75ch] text-lg text-white lg:text-2xl">
           From unlimited Signature Classes, unparalleled Personal Training, to
           iconic locations with exclusive amenities, Equinox membership unlocks
           everything you need to maximize your potential.
