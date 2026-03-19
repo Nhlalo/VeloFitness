8a253cfd9a57be6af76313ae9b4d0f49bdad6798
Nhlalo <nhlalonkosi@gmail.com>
2026-03-19 11:59:41 +0200

    feat: create select club component

---
+++ src/pages/jointoday/JoinToday.tsx
@@ -0,0 +1,26 @@
+function SelectClub() {
+  return (
+    <div>
+      <div>
+        {/* Three buttons container */}
+
+        <div>
+          <h2>3 Steps you are in</h2>
+
+          <form>
+            <div>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Surname" />
+            </div>
+
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Zip Code" />
+            <input type="tel" placeholder="Phone Number" />
+
+            <button type="submit">Select a club +</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
