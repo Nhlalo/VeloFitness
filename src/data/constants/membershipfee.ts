323b577eb03ecf380d62b534f232624a8cc0ab63
Nhlalo <nhlalonkosi@gmail.com>
2026-03-25 13:04:20 +0200

    feat: add membership fee data

---
+++ src/data/constants/membershipfee.ts
@@ -0,0 +1,26 @@
+import PremiumClub from "../../assets/images/jointoday-premiumgym.jpg";
+import EliteClub from "../../assets/images/jointoday-elitemembership.jpg";
+import StandardClub from "../../assets/images/jointoday-signature.jpg";
+
+const membershipData = [
+  {
+    image: PremiumClub,
+    title: "Premium Club Access",
+    club: "Le Cercle d'Or", // The Golden Circle
+    price: "$79",
+  },
+  {
+    image: EliteClub,
+    title: "Elite Member Benefits",
+    club: "La Société Privée", // The Private Society
+    price: "$99",
+  },
+  {
+    image: StandardClub,
+    title: "Signature Experience",
+    club: "L'Ordre des Champions", // The Order of Champions
+    price: "$129",
+  },
+];
+
+export { membershipData };
