1338ab6bc10a2505cbafd164ab25d66db1098248
Nhlalo <nhlalonkosi@gmail.com>
2026-04-08 18:33:18 +0200

    feat: create membership header component

---
+++ src/pages/dashboard/membershipoverview/MembershipHeader.tsx
@@ -0,0 +1,14 @@
+export default function MembershipHeader() {
+  return (
+    <div>
+      <div>
+        <div>Your Plan</div>
+        <h1>Membership</h1>
+      </div>
+      <div>
+        <button>Change Plan</button>
+        <button>Cancel Subscription</button>
+      </div>
+    </div>
+  );
+}
