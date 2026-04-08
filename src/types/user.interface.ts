466e80e195331f6fc23246a0b17f049b451011e2
Nhlalo <nhlalonkosi@gmail.com>
2026-04-08 18:24:54 +0200

    feat: create an interface

---
+++ src/types/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  name: string;
+  surname: string;
+  email: string;
+  zipCode: string;
+  phoneNumber: string;
+  selectedClub: string;
+  memberSince: string;
+  avatarInitials: string;
+}
