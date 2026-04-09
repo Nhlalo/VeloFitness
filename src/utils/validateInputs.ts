44ab61e9142b2b9aa06bc1e9021b6dbce2bc63b2
Nhlalo <nhlalonkosi@gmail.com>
2026-04-09 17:14:55 +0200

    feat: implement a function to test the validity of an input

--- src/utils/validateInputs.ts
+++ src/utils/validateInputs.ts
@@ -11,3 +11,10 @@ export default function validateField<T extends Record<string, any>>(
     [fieldName]: !isValid && value.length > 0,
   }));
 }
+
+function isInputValid(pattern: string, value: string) {
+  const regex = new RegExp(pattern);
+  const isValid = regex.test(value);
+  return isValid;
+}
+export { isInputValid };
