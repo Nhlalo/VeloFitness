9ad87860ddabefd2df1caf5889977c522fe5cb0f
Nhlalo <nhlalonkosi@gmail.com>
2026-04-09 19:22:51 +0200

    refactor remove unnecessary code

--- src/pages/signin/SignInForm.tsx
+++ src/pages/signin/SignInForm.tsx
@@ -162,7 +162,6 @@ export default function SignInForm({ onForgotPassword }: SignInFormProps) {
         .
       </div>
 
-      {/* Sign In Button */}
       <button
         type="submit"
         disabled={!isFormValid}
