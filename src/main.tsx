3a85870841a6567f4cf434aa6a7244997bac0a2d
Nhlalo <nhlalonkosi@gmail.com>
2026-04-13 17:16:37 +0200

    feat: create a global login state using context api

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./styles/global.css";
 import { router } from "./route";
+import AuthProvider from "./context/authContext";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
