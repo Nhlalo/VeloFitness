d55c3c2877d4695d2f6b49fd27124aa605592d45
Nhlalo <nhlalonkosi@gmail.com>
2026-04-13 21:35:06 +0200

    style: make the subclasses component responsive

--- src/components/shared/SubClasses.tsx
+++ src/components/shared/SubClasses.tsx
@@ -12,27 +12,29 @@ export default function SubClasses({
 
   return (
     <Container>
-      <div className="m-auto flex flex-col py-18 lg:flex-row">
-        <div>
+      <div className="m-auto flex flex-col py-18 lg:flex-row lg:gap-8">
+        {/* Add w-full and lg:w-1/2 to both divs */}
+        <div className="w-full lg:w-1/2">
           <h2 className="mt-5 mb-6 text-3xl font-bold uppercase lg:text-4xl">
             {heading}
           </h2>
-          <p className="mb-6 hidden w-[60%] py-3 text-lg leading-[1.4] lg:block">
+          <p className="mb-6 hidden w-full py-3 text-lg leading-[1.4] lg:block lg:w-[90%]">
             {description}
           </p>
           <button
             onClick={handleClick}
-            className="m-2 hidden rounded-md border-2 border-white bg-transparent px-8 py-4 text-white hover:bg-gray-400 lg:inline"
+            className="m-2 hidden rounded-md border-2 border-white bg-transparent px-8 py-4 text-white hover:bg-gray-400 lg:inline-block"
           >
             BOOK A CLASS
           </button>
         </div>
-        <div className="flex-1 overflow-hidden lg:max-w-[80%]">
+
+        <div className="w-full lg:w-1/2">
           <img
             src={imageSource}
             alt="training"
             loading="lazy"
-            className="w-full"
+            className="h-auto w-full"
           />
           <p className="mb-6 py-5 lg:hidden">{description}</p>
           <button className="m-2 rounded-md border-2 border-white bg-transparent px-5 py-3 text-white hover:bg-gray-400 lg:hidden">
