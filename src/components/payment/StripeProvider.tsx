be7de3d3b198e6e7748356834546f2f45b92a881
Nhlalo <nhlalonkosi@gmail.com>
2026-04-21 15:32:32 +0200

    feat: create StripeProvider component

---
+++ src/components/payment/StripeProvider.tsx
@@ -0,0 +1,10 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "../../config/stripe";
+
+export default function StripeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Elements stripe={stripePromise}>{children}</Elements>;
+}
